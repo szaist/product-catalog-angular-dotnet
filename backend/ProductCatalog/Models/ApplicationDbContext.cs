4ab842cf3b04e58b93e2a1b9093c8da3acbc1a86
DESKTOP-D7KB0N5\Bence-PC <egyjatekos0204@gmail.com>
2024-03-06 14:42:18 +0100

    .Net project init, ApplicationDbContext added, Product Entity created

---
+++ backend/ProductCatalog/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Models.Entities;
+
+namespace ProductCatalog.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ backend/ProductCatalog/Models/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductCatalog.Models.Entities
+{
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
